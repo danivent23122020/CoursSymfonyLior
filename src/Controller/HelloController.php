e7d4cb88f0731d1ede55d4cecf20dbbec774e804
Danivent <dan.vent@laposte.net>
2021-04-20 09:49:21 +0200

    commit

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -7,18 +7,31 @@ use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
 class HelloController{
-
+    
     // route hello
     /**
      * @Route("/hello/{prenom?World}", name="hello", methods={"GET", "POST"})
      */
     public function hello($prenom = "World", Environment $twig){
         
-        $html = $twig->render('hello.html.twig',
-    [
-        'prenom' => $prenom,
-    ]
-    );
-        return new Response($html);
+            $html = $twig->render('hello.html.twig',
+        [
+            'prenom' => $prenom,
+        ]
+        );
+            return new Response($html);
+    }
+
+    /**
+     * @Route("/example", name="example")
+     */
+    public function example(Environment $twig){
+        
+            $html = $twig->render('example.html.twig',
+        [
+            'age' => 33,
+        ]
+        );
+            return new Response($html);
     }
 }
