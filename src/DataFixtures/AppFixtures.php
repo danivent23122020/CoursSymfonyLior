110ca8abf9884edfa71e33c09f8fb280bb34e24d
Danivent <dan.vent@laposte.net>
2021-04-21 18:31:59 +0200

    commit

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,13 +2,23 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Product;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        for($p = 0 ; $p < 100 ; $p++)
+        {
+            $product = new Product;
+            $product->setName("Product");
+            $product->setPrice(mt_rand(100, 200));
+            $product->setSlug("produit-n-$p");
+
+            $manager->persist($product);
+        }
         // $product = new Product();
         // $manager->persist($product);
 
