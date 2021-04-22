4fb01374dfe4df80a6c6977745609309f4dd82fb
Danivent <dan.vent@laposte.net>
2021-04-22 11:58:24 +0200

    commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController{
     
-
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage(){
-        return $this->render('home.tml.twig');
+    public function homepage(ProductRepository $productRepository){
+        
+        $products = $productRepository->findBy([], [], 3);
+        
+        return $this->render('home.tml.twig', [
+            'products' => $products
+        ]);
     }
 }
