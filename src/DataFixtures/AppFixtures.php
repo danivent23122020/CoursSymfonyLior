9d946265bd2dd5cee32590711f8b2d506e811571
Danivent <dan.vent@laposte.net>
2021-04-27 10:23:46 +0200

    commit

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,16 +5,20 @@ namespace App\DataFixtures;
 use App\Entity\Category;
 use Faker\Factory;
 use App\Entity\Product;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
     protected $slugger;
-    public function __construct(SluggerInterface $slugger)
+    protected $encoder;
+    public function __construct(SluggerInterface $slugger, UserPasswordEncoderInterface $encoder)
     {
         $this->slugger = $slugger;
+        $this->encoder = $encoder;
     }
     
     public function load(ObjectManager $manager)
@@ -25,6 +29,32 @@ class AppFixtures extends Fixture
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
         $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
 
+        // generate admin
+        $admin = new User;
+        // hachage du password
+        $hash = $this->encoder->encodePassword($admin, "password");
+        // set admin
+        $admin->setEmail("admin@gmail.com")
+        ->setFullName('Admin')
+        ->setPassword($hash)
+        ->setRoles([
+            'ROLE_ADMIN'
+        ]);
+        $manager->persist($admin);
+        
+        // generate users
+        
+        for($u = 0 ; $u < 5 ; $u++){
+            $user = new User();
+            // hachage du password
+            $hash = $this->encoder->encodePassword($user, "password");
+            $user->setEmail("user$u@gmail.com")
+            ->setFullName($faker->name())
+            ->setPassword($hash);
+            $manager->persist($user);
+        }
+
+        // generate category
         for($c = 0; $c < 3;$c++)
         {
             $category = new Category;
