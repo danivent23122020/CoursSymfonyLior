459cadd418f30ec5f8dc92cb138ff62aeca92471
Danivent <dan.vent@laposte.net>
2021-04-30 10:41:12 +0200

    commit

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -63,7 +63,7 @@ class LoginFormAuthenticator extends AbstractGuardAuthenticator
 
     public function start(Request $request, AuthenticationException $authException = null)
     {
-        // todo
+        return new RedirectResponse('/login');
     }
 
     public function supportsRememberMe()
